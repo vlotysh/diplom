0cb25c55c4b52534ae54572a25ff203f794f5072
Vlad <Gangsta1@i.ua>
2014-02-11 14:51:28 +0200

    Добавлена функция cyr2lat к хелперу HTML

--- application/views/user/user.php
+++ application/views/user/user.php
@@ -1,3 +1,5 @@
 <p><?=$user_info->username;?> ------ <?if($online == TRUE):?><span style="color: tomato;">online</span><?else:?><span>Заходил <?=HTML::rusTime($user_info->last_activity);?></span><?endif;?></p> 
 <p><?=$massege?></p>
 <?=$massege_form;?>
+
+<!--<p><?=HTML::cyr2lat('Какая-то строчка');?></p>-->
