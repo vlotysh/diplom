2e514e28d58c76d094fc3af787c6bb799aabb073
VladiKiller <Gangsta1@i.ua>
2014-07-01 15:04:19 +0300

    ajax test

--- application/views/user/mail_index.php
+++ application/views/user/mail_index.php
@@ -1,76 +1,73 @@
 
 <ul class="pm_section">
-    <li class="<?=$inbox?>">
-        <a class="ajax" href="<?=  URL::base('').'mail'?>">Входящие</a>
+    <li class="<?= $inbox ?>">
+        <a class="ajax" href="<?= URL::base('') . 'mail' ?>">Входящие</a>
     </li>
-    <li class="<?=$outbox?>">
-        <a class="ajax" href="<?=   URL::base('').'mail/?section=outbox'?>">Исходящие</a>
+    <li class="<?= $outbox ?>">
+        <a class="ajax" href="<?= URL::base('') . 'mail/?section=outbox' ?>">Исходящие</a>
     </li>
-   
-    </ul>
-<?php echo $pagination;?>
+
+</ul>
+<?php echo $pagination; ?>
 
 <div class="mail_cell">
     <table width="100%" class="mail_table"> 
-        <?= $privet_ms;?>
-  
-     </table>
-   
-    <?php echo $pagination;?>
-    </div>
+        <?= $privet_ms; ?>
+
+    </table>
+
+    <?php echo $pagination; ?>
+</div>
 
 
 <script>
-         
-        function changeHash(id) {
 
-try {
-  // history.replaceState(null, "",id);
-    window.history.pushState(id, "", id); 
-}
-catch(e) {
-   location.hash = '#id_'+id;
-}
+    function changeHash(id) {
 
-}
-$('a.ajax').on('click',function() {
+        try {
+            // history.replaceState(null, "",id);
+            window.history.pushState(id, "", id);
+        }
+        catch (e) {
+            location.hash = '#id_' + id;
+        }
 
-    
-    var href = $(this).attr('href');
-    changeHash(href);
-   
-    
-   /*  $('.cont').load(href);*/
-    $.ajax({  
-                url: href,
-                type: "POST",
-                cache: false,  
-                success: function(res){
-                var data = $.parseJSON(res);
-                   $('.cont').html(data.content);
-                   
-                }
-
-})
-
- return false;
-
-})
-
-window.onpopstate = function(e) {
-    if(e.state != null) {
-        var href = e.state;
-         $.ajax({  
-                url: href,
-                type: "POST",
-                cache: false,  
-                success: function(res){
-                   var data = $.parseJSON(res);
-                   $('.cont').html(data.content);
-                   
-                }
-})
     }
-}
+    $('a.ajax, .pagination a').on('click', function() {
 
-        </script>
+        var href = $(this).attr('href');
+        changeHash(href);
+
+
+        /*  $('.cont').load(href);*/
+        $.ajax({
+            url: href,
+            type: "POST",
+            cache: false,
+            success: function(res) {
+                var data = $.parseJSON(res);
+                $('.cont').html(data.content);
+
+            }
+
+        })
+        return false;
+    })
+
+    window.onpopstate = function(e) {
+        if (e.state != null) {
+            var href = e.state;
+            $.ajax({
+                url: href,
+                type: "POST",
+                cache: false,
+                success: function(res) {
+                    var data = $.parseJSON(res);
+                    $('.cont').html(data.content);
+
+                }
+            })
+        }
+    }
+
+</script>
