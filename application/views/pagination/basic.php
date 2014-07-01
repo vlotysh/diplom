568a4288897fba4bf59f19086011da3bfb101afc
VladiKiller <Gangsta1@i.ua>
2014-07-01 21:21:11 +0300

    fix

--- application/views/pagination/basic.php
+++ application/views/pagination/basic.php
@@ -1,19 +1,13 @@
 <div  class="pagination">
     <ul>
 
-        <?php if ($first_page !== FALSE): ?>
-            <li><a href="<?php echo HTML::chars($page->url($first_page)) ?>" rel="first"><?php echo __('First') ?></a></li>
-        <?php else: ?>
-            <!--<?php echo __('First') ?>-->
-            <li><a href="#" style="color:black"><?php echo __('First') ?></a></li>
-        <?php endif ?>
+
 
         <?php if ($previous_page !== FALSE): ?>
             <li><a href="<?php echo HTML::chars($page->url($previous_page)) ?>" rel="prev"><?php echo __('Previous') ?></a></li>
-        <?php else: ?>
-            <!--<?php echo __('Previous') ?>-->
-            <li><a href="#" style="color:black"><?php echo __('Previous') ?></a></li>
         <?php endif ?>
+           
+        
 
         <?php for ($i = 1; $i <= $total_pages; $i++): ?>
 
@@ -28,17 +22,10 @@
 
         <?php if ($next_page !== FALSE): ?>
             <li><a href="<?php echo HTML::chars($page->url($next_page)) ?>" rel="next"><?php echo __('Next') ?></a></li>
-        <?php else: ?>
-            <!--<?php echo __('Next') ?>-->
-            <li><a href="#" style="color:black"><?php echo __('Next') ?></a></li>
         <?php endif ?>
+            
 
-        <?php if ($last_page !== FALSE): ?>
-            <li><a href="<?php echo HTML::chars($page->url($last_page)) ?>" rel="last"><?php echo __('Last') ?></a></li>
-        <?php else: ?>
-            <!--<?php echo __('Last') ?>-->
-            <li><a href="#" style="color:black"><?php echo __('Last') ?></a></li>
-            <?php endif ?>
+ 
 
     </ul><!-- .pagination -->
 
