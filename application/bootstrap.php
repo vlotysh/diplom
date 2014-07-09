5119489c906d76139cdcebdece298a4a873e1150
VladiKiller <Gangsta1@i.ua>
2014-07-09 20:43:03 +0300

    try

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -142,7 +142,7 @@ Session::$default = 'database';
  * Set the default language
  */
 // check cookie first
-$lang = Cookie::get('lang');
+$lang = 'ru';
 
 // if no cookie, rely on accept_language
 if (empty($lang)) {
